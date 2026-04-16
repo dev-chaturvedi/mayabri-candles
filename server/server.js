4d309f995b1cdefa9aba94ac4f3b426905cf32fc
Abhay Chaturvedi <57232710+dev-chaturvedi@users.noreply.github.com>
2026-04-16 19:04:54 +0530

    Update server.js enable the cors

--- server/server.js
+++ server/server.js
@@ -17,12 +17,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.API_PORT || 5000;
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: ["https://mayabri-candles.vercel.app", "http://localhost:3000"],
+  credentials: true
+}));
 
 app.post(
   "/api/checkout/webhook",
